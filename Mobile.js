0a644913702cb16a1e64f0404a2790004aaa0496
HoangDT <49191532+hoangdtcg@users.noreply.github.com>
2021-04-22 16:33:11 +0700

    Add files via upload

---
+++ Mobile.js
@@ -0,0 +1,30 @@
+class Mobile {
+    constructor(_name,_type) {
+        this.name = _name;
+        this.type = _type;
+        this.battery = 100;
+        this.message = '';
+        this.inbox = [];
+        this.outbox = [];
+    }
+
+    writeMsg(msg){
+        this.message = msg;
+    }
+
+    receiveMsg(msg){
+        this.inbox.push(msg);
+    }
+
+    sendMsg(mobile){
+        mobile.receiveMsg(this.message);
+        this.outbox.push(this.message);
+    }
+
+    decreaseBattery(){
+        if(this.battery > 0)
+            this.battery-=20;
+        else
+            alert("Sạc pin vào bạn êiiii!");
+    }
+}
